790e240aeb3990dcbb98f2b72c3db5d6f2617e3d
David <dudiko2@gmail.com>
2021-07-13 01:53:59 +0300

    basic hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,33 @@
 import { FC } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Link from "next/link";
+import styles from "../styles/Hero.module.css";
 
 const Hero: FC = () => {
 	return (
-		<h1>
-			what <p>ha</p>
-		</h1>
+		<Container fluid className={styles.Hero}>
+			<Container>
+				<Row>
+					<Col lg={8}>
+						<div className={styles.content}>
+							<span className={`uppercase`}>subtitle</span>
+							<h1 className={`capitalize ${styles.heading}`}>
+								for every occassion
+							</h1>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+								nemo aut repudiandae!
+							</p>
+							<Link href="/" passHref>
+								<Button className={`uppercase ${styles.button}`}>
+									shop now
+								</Button>
+							</Link>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		</Container>
 	);
 };
 
--- hoc/Layout.tsx
+++ hoc/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import styles from "../styles/Layout.module.css";
 
 interface Props {
 	children?: ReactNode;
@@ -8,11 +9,11 @@ interface Props {
 
 const Layout: FC<Props> = ({ children }) => {
 	return (
-		<>
+		<div className={styles.Layout}>
 			<Header />
-			<main>{children}</main>
+			<div>{children}</div>
 			<Footer />
-		</>
+		</div>
 	);
 };
 
