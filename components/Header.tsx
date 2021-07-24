e8d3d3dd9385995619146aa13da03e5fa01e32af
David <dudiko2@gmail.com>
2021-07-25 01:52:12 +0300

    mobile menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,37 +1,41 @@
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
-import { FC } from "react";
+import { FC, useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 
 // NOTE: Revise links
 
 const Header: FC = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<Navbar expand="lg" className={`${styles.Navbar}`}>
 			<Link passHref href="/">
 				<Navbar.Brand>MYSHOP</Navbar.Brand>
 			</Link>
-			<Navbar.Toggle />
-			<Navbar.Collapse>
-				<div className={styles.collapseable}>
-					<Nav className={`${styles.Navlinks} capitalize`}>
-						<Nav.Item>
-							<Link passHref href="/">
-								<Nav.Link>collections</Nav.Link>
-							</Link>
-						</Nav.Item>
-						<Nav.Item>
-							<Nav.Link>about</Nav.Link>
-						</Nav.Item>
-					</Nav>
-					<Form inline>
-						<div className={`input-cont`}>
-							<FormControl type="text" placeholder="Search" />
-							<Button>B</Button>
-						</div>
-					</Form>
-				</div>
-			</Navbar.Collapse>
+			<div className={`${styles.collapseable} ${isOpen ? styles.show : ""}`}>
+				<Nav className={`${styles.Navlinks} capitalize`}>
+					<Nav.Item>
+						<Link passHref href="/">
+							<Nav.Link>collections</Nav.Link>
+						</Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link>about</Nav.Link>
+					</Nav.Item>
+				</Nav>
+				<Form inline>
+					<div className={`input-cont`}>
+						<FormControl type="text" placeholder="Search" />
+						<Button>B</Button>
+					</div>
+				</Form>
+			</div>
+			<Navbar.Toggle className={styles.front} onClick={toggleMenu} />
 		</Navbar>
 	);
 };
