e5987a85ac0f33afcf51c9dad3aad588028ed6af
David <dudiko2@gmail.com>
2021-09-29 23:55:49 +0300

    .

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -8,37 +8,46 @@ import { searchKeys } from "../../components/SearchBar";
 
 interface Props {
 	products: ShopifyProduct[];
+	querystring: string;
 }
 
-const Products: FC<Props> = ({ products }) => {
+const Products: FC<Props> = ({ products, querystring = null }) => {
+	// note: add support for pagination
+
 	return (
 		<Layout>
 			<Head>
 				<title>Products</title>
 			</Head>
-			<ItemGallery products={products} />
+			{products.length ? (
+				<ItemGallery products={products} />
+			) : (
+				`No results for '${querystring}'`
+			)}
 		</Layout>
 	);
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-	const querystring = getOneQueryParam(query[searchKeys.QUERY]);
-	const sortKey = getOneQueryParam(query[searchKeys.SORT_KEY])?.toUpperCase();
+	const querystring = getOneQueryParam(query[searchKeys.QUERY]) || "";
+	const sortKey =
+		getOneQueryParam(query[searchKeys.SORT_KEY])?.toUpperCase() || "TITLE";
 
 	const products = await fetchProductsAndSortBy({
-		queryString: querystring || "",
-		sortKey: sortKey || "TITLE",
+		queryString: querystring,
+		sortKey: sortKey,
 	});
 
 	return {
 		props: {
 			products,
+			querystring,
 		},
 	};
 };
 
 const getOneQueryParam = (param?: string | Array<string>) => {
-	if (!param) return undefined;
+	if (!param) return null;
 
 	return param?.constructor === Array ? param[0] : (param as string);
 };
